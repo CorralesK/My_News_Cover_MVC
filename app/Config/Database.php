9667dbb2af06d3e033b63c36b2a38e33c53e541e
CorralesK <kimcorrales@gmail.com>
2023-11-17 13:49:36 -0600

    database configuration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'root',
+        'password'     => 'Admin12345',
+        'database'     => 'my_news_cover',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
