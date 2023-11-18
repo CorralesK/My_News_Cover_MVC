266818b4d21df78c3d39ed609b6d03889dca6041
CorralesK <kimcorrales@gmail.com>
2023-11-17 18:34:39 -0600

    Added the routes for the category crud

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,13 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('categories', 'Categories::index');
+
+$routes->get('categories/add', 'Categories::addCategory');
+$routes->post('categories/store', 'Categories::storeCategory');
+
+$routes->get('categories/edit/(:num)', 'Categories::editCategory/$1');
+$routes->post('categories/update', 'Categories::updateCategory');
+
+$routes->get('categories/delete/(:num)', 'Categories::deleteCategory/$1');
