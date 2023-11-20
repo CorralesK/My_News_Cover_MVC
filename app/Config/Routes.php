1b889b85e7ef2595c808b737f5fcfdf4ab7f16fd
CorralesK <kimcorrales@gmail.com>
2023-11-19 19:00:28 -0600

    Added routes for news sources

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,10 +10,21 @@ $routes->get('/', 'Home::index');
 //Categories
 $routes->get('categories', 'Categories::index');
 
-$routes->get('categories/create', 'Categories::create');
+$routes->get('categories/create', 'Categories::createCategory');
 $routes->post('categories/store', 'Categories::store');
 
-$routes->get('categories/edit/(:num)', 'Categories::edit/$1');
+$routes->get('categories/edit/(:num)', 'Categories::editCategories/$1');
 $routes->post('categories/update', 'Categories::update');
 
 $routes->get('categories/delete/(:num)', 'Categories::delete/$1');
+
+//News Sources
+$routes->get('newsSources', 'NewsSources::index');
+
+$routes->get('newsSources/create', 'NewsSources::createSource');
+$routes->post('newsSources/store', 'NewsSources::store');
+
+$routes->get('newsSources/edit/(:num)', 'NewsSources::editSource/$1');
+$routes->post('newsSources/update', 'NewsSources::update');
+
+$routes->get('newsSources/delete/(:num)', 'NewsSources::delete/$1');
