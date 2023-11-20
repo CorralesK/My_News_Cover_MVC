194dafafa4d5e2c201627222cd3b4df90a2fe443
CorralesK <kimcorrales@gmail.com>
2023-11-19 19:12:12 -0600

    Created the list and add view

---
+++ app/Views/Users/NewsSources/index.php
@@ -0,0 +1,32 @@
+<?= $this->extend('templates/main') ?>
+
+<?= $this->section('content') ?>
+<div class="container w-75">
+    <h1 class="mt-5 mb-4 h2">Fuentes de Noticias</h1>
+
+    <table class="table table-striped table-hover table-bordered mb-4 w-75">
+        <thead>
+            <tr>
+                <th>Nombre de la fuente</th>
+                <th>Categoría</th>
+                <th>Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($sources as $source): ?>
+                <tr>
+                    <td><?= esc($source['name']); ?></td>
+                    <td><?= esc($source['categoryName']); ?></td>
+                    <td>
+                        <a class="text-warning text-decoration-none" href="<?php echo site_url('newsSources/edit/' . $source['id']); ?>">Editar</a> |
+                        <a class="text-danger text-decoration-none" href="<?php echo site_url('newsSources/delete/' . $source['id']); ?>" onclick="return confirm('¿Seguro que deseas eliminar a <?= esc($source['name']); ?>?');">Eliminar</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <a href="<?php echo site_url('newsSources/create'); ?>" type="btn" class="btn btn-primary">Agregar</a>
+
+</div>
+<?= $this->endSection() ?>
