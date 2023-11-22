8fd78490ef9578c4734348dc75bb36fc9a02b2f4
CorralesK <kimcorrales@gmail.com>
2023-11-21 19:15:47 -0600

    Creation of routes for user authentication processes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+ //Users
+$routes->get('login', 'Users::index');
+$routes->post('auth/login', 'Users::login');
+
+$routes->get('signup', 'Users::signup');
+$routes->post('auth/store', 'Users::store');
+
+$routes->get('logout', 'Users::logout');
 
 //Categories
 $routes->get('categories', 'Categories::index');
@@ -25,6 +33,6 @@ $routes->get('newsSources/create', 'NewsSources::create');
 $routes->post('newsSources/store', 'NewsSources::store');
 
 $routes->get('newsSources/edit/(:num)', 'NewsSources::edit/$1');
-$routes->post('newsSources/update', 'NewsSources::update');
+$routes->post('newsSources/update/(:num)', 'NewsSources::update/$1');
 
 $routes->get('newsSources/delete/(:num)', 'NewsSources::delete/$1');
